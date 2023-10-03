ae9e8d8c7006789e136efbe235b2761894e90270
Yurii <gapirov21@gmail.com>
2023-10-03 11:05:34 +0300

    first commit data

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import Notiflix from 'notiflix';
+// import axios, { isCancel, AxiosError } from 'axios';
+// import SimpleLightbox from "simplelightbox";
+
+// Your API key: 39807884-ce16e485562f7e433cd996b10
+// base url https://pixabay.com/api/
