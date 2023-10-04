73c5e30d668705d4c709f7d60e12d3b26b7786ba
Yurii <gapirov21@gmail.com>
2023-10-05 00:08:06 +0300

    some fix lightbox

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ let currentPage = 1; // Початкова сторінка
 const imagesPerPage = 40; // Кількість зображень на сторінці
 let totalLoadedImagesCount = 0; // Лічильник завантажених зображень
 let searchQuery;
-let lightbox;
+let lightBox;
 let totalHits = 0;
-
 
 export const selectors = {
     searchForm: document.getElementById('search-form'),
@@ -40,23 +39,23 @@ async function onFormSubmit(evt) {
     const { hits, totalHits } = await fetchImages(searchQuery, currentPage);
     // totalLoadedImagesCount += hits.length;
     const imgListHTML = createImgList(hits);
-
+    
     // Вставляємо HTML-код зображень в галерею
     selectors.gallery.insertAdjacentHTML('beforeend', imgListHTML);
-
-    // if (totalLoadedImagesCount < totalHits) {
-    //     selectors.loadMoreBtn.style.display = 'block';
-    // }
     
+    if (!lightBox) {
+        lightBox = new SimpleLightbox('.gallery a');
+    } else {
+        lightBox.refresh();
+    }
+
     if (totalHits > 0) {
         selectors.loadMoreBtn.style.display = 'block';
         Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
     } else {
         Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.", { position: 'center-top', distance: '200px' });
     }
-     if (!lightbox) {
-        lightbox = new SimpleLightbox('.gallery a');
-    }
+    
 }
 
 async function onLoadMore() {
@@ -65,12 +64,16 @@ async function onLoadMore() {
     const { hits } = await fetchImages(searchQuery, currentPage);
     totalLoadedImagesCount += hits.length;
     const additionalImagesHTML = createImgList(hits);
-
+    lightbox.refresh();
     if (additionalImagesHTML) {
         // Вставляємо HTML-код додаткових зображень в кінець галереї
         selectors.gallery.insertAdjacentHTML('beforeend', additionalImagesHTML);
-        lightbox.refresh();
 
+        if (!lightBox) {
+            lightBox = new SimpleLightbox('.gallery a');
+        } else {
+            lightBox.refresh();
+        }
         // Прокручуємо сторінку плавно після завантаження нових зображень
         const { height: cardHeight } = document
             .querySelector(".gallery")
