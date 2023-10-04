1cc7d48433f13e1137b73e4cc5a67ed4f5b9f0d3
Yurii <gapirov21@gmail.com>
2023-10-04 16:00:10 +0300

    new code

--- src/index.js
+++ src/index.js
@@ -1,66 +1,98 @@
+
 import { fetchImages } from './img-api';
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
 
 let currentPage = 1; // Початкова сторінка
 const imagesPerPage = 40; // Кількість зображень на сторінці
 let totalLoadedImagesCount = 0; // Лічильник завантажених зображень
+let searchQuery;
+let lightbox;
 
 export const selectors = {
     searchForm: document.getElementById('search-form'),
     gallery: document.querySelector('.gallery'),
     loadMoreBtn: document.querySelector('.load-more'),
-    }
+}
 
 selectors.loadMoreBtn.style.display = 'none';
- 
-selectors.searchForm.addEventListener('submit', onSearch);
 
-function onSearch(evt) {
+selectors.searchForm.addEventListener('submit', onFormSubmit);
+selectors.loadMoreBtn.addEventListener('click', onLoadMore);
+
+async function onFormSubmit(evt) {
     evt.preventDefault();
     selectors.loadMoreBtn.style.display = 'none';
-    
-    fetchAndDisplayImages();
+    currentPage = 1;
+    searchQuery = selectors.searchForm.searchQuery.value.trim(); // Отримуємо значення з поля вводу
 
-}
+    if (!searchQuery) {
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.", { position: 'center-top', distance: '200px' });
+        return;
+    }
 
-async function fetchAndDisplayImages() {
-    const searchQuery = selectors.searchForm.searchQuery.value.trim(); // Отримуємо значення з поля вводу
-    const imageListHTML = await fetchImages(searchQuery, currentPage);
+    // Очищаємо галерею перед новим пошуком
+    selectors.gallery.innerHTML = '';
 
-    if (imageListHTML) {
-        selectors.gallery.innerHTML = imageListHTML;
-        const lightbox = new SimpleLightbox('.gallery a');
+    const { hits, totalHits } = await fetchImages(searchQuery, currentPage);
+    totalLoadedImagesCount += hits.length;
+    const imgListHTML = createImgList(hits);
+
+    // Вставляємо HTML-код зображень в галерею
+    selectors.gallery.insertAdjacentHTML('beforeend', imgListHTML);
+
+    if (totalLoadedImagesCount < totalHits) {
         selectors.loadMoreBtn.style.display = 'block';
-
-        lightbox.refresh();
+    }
+     if (!lightbox) {
+        lightbox = new SimpleLightbox('.gallery a');
     }
 }
-
-selectors.loadMoreBtn.addEventListener('click', onLoadMore);
 
 async function onLoadMore() {
     currentPage++; // Збільшуємо номер сторінки
     const searchQuery = selectors.searchForm.searchQuery.value.trim();
-    const additionalImagesHTML = await fetchImages(searchQuery, currentPage);
+    const { hits } = await fetchImages(searchQuery, currentPage);
+    totalLoadedImagesCount += hits.length;
+    const additionalImagesHTML = createImgList(hits);
 
     if (additionalImagesHTML) {
-        selectors.gallery.innerHTML += additionalImagesHTML; // Додаємо нові зображення до вже існуючих
-        const lightbox = new SimpleLightbox('.gallery a');
+        // Вставляємо HTML-код додаткових зображень в кінець галереї
+        selectors.gallery.insertAdjacentHTML('beforeend', additionalImagesHTML);
         lightbox.refresh();
 
         // Прокручуємо сторінку плавно після завантаження нових зображень
         const { height: cardHeight } = document
             .querySelector(".gallery")
             .firstElementChild.getBoundingClientRect();
-        
+
         window.scrollBy({
             top: cardHeight * 2,
             behavior: "smooth",
         });
 
+        // Приховуємо кнопку "Load More", якщо всі зображення завантажені
+        if (totalLoadedImagesCount >= totalHits) {
+            selectors.loadMoreBtn.style.display = 'none';
+        }
     }
 }
-selectors.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function createImgList(arr) {
+    const imgListHTML = arr.map(item => `
+        <div class="photo-card">
+            <a href="${item.largeImageURL}" data-lightbox="image">
+                <img src="${item.webformatURL}" width="500" alt="${item.tags}" loading="lazy" />
+            </a>
+            <div class="info">
+                <p class="info-item"><b>Likes</b> ${item.likes}</p>
+                <p class="info-item"><b>Views</b> ${item.views}</p>
+                <p class="info-item"><b>Comments</b> ${item.comments}</p>
+                <p class="info-item"><b>Downloads</b> ${item.downloads}</p>
+            </div>
+        </div>
+    `).join('');
+
+    return imgListHTML;
+}
